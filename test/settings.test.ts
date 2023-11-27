a54680e00686a44ed73984742b3e7019d82cbb69
mfw78 <mfw78@protonmail.com>
2023-11-27 08:48:23 +0000

    feat: quorum

--- test/settings.test.ts
+++ test/settings.test.ts
@@ -11,4 +11,7 @@ describe("settings.json", () => {
   test("Spam protection: require 10000 COW/vCOW", () => {
     expect(settings.validation.params.minScore == 10000).toBe(true);
   });
+  test("Participation: require 35M COW/vCOW quorum", () => {
+    expect(settings.voting.quorum == 35000000).toBe(true);
+  })
 });
