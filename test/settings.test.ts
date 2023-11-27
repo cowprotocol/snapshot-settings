b502cf9d2ffb3adb087a19b66da9447b6d614475
mfw78 <53399572+mfw78@users.noreply.github.com>
2023-11-27 14:58:43 +0000

    Productionise snapshot settings (#5)
    * test: spam protection
    
    * feat: quorum
    
    * chore: repo devcontainer
    
    * cicd: auto pin ipfs
    
    * fix: prettier
    
    * cicd: test other ipfs action
    
    * cicd: publish to ipfs
    
    * docs: readme
    
    * Update test/settings.test.ts
    
    Co-authored-by: Federico Giacon <58218759+fedgiac@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Federico Giacon <58218759+fedgiac@users.noreply.github.com>

--- test/settings.test.ts
+++ test/settings.test.ts
@@ -8,4 +8,10 @@ describe("settings.json", () => {
       snapshot.utils.validateSchema(snapshot.schemas.space, settings),
     ).toBe(true);
   });
+  test("Spam protection: require 10000 COW/vCOW", () => {
+    expect(settings.validation.params.minScore).toBe(10_000);
+  });
+  test("Participation: require 35M COW/vCOW quorum", () => {
+    expect(settings.voting.quorum).toBe(35_000_000);
+  });
 });
